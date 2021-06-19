93787ad79bc02d2156bab6a193062b664ea19e0f
한명수 <hmsoo@hanmyeongsuui-MacBookPro.local>
2021-06-19 23:32:46 +0900

    rollback mysql

--- servers/main.js
+++ servers/main.js
@@ -3,13 +3,14 @@ const app = express();
 const path = require('path');
 const url = require('url');
 const bodyParser = require('body-parser');
-const mariadb = require('mariadb');
+const mysql = require('mysql');
+const { connect } = require('http2');
 
-const pool = mariadb.createPool({
+const connection = mysql.createConnection({
 	host: '34.64.121.246',
 	user: 'nobot',
 	password: 'nobotgproject',
-	port: 3308,
+	port: 3306,
 	database: 'gproject',
 });
 
@@ -19,16 +20,22 @@ app.get('/', function (req, res) {
 	res.send('hi');
 });
 
-app.post('/detect', async function (req, res) {
+app.post('/detect', function (req, res) {
 	let data = JSON.parse(Object.keys(req.body)[0]);
 	let i = 1;
-	console.log(data[0]);
-	const connection = await pool.getConnection();
-
-	const result = await connection.query(
-		`INSERT INTO dataset (set_no,location_tag) VALUES (${data[0].timestamp},${data[0].location_tag})`
+	console.log(data);
+	connection.connect;
+	connection.query(
+		`INSERT INTO dataset (set_no,location_tag) VALUES (${data[0].timestamp},${data[0].location_tag})`,
+		(err, rows, fields) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(rows.set_no);
+			}
+		}
 	);
-	console.log(result[0]);
+
 	// for (i in data) {
 	// 	connection.query(
 	// 		`INSERT INTO data_array (location_tag,dataset_no,x_location,y_location) VALUES (${data[0].location_tag},${data[0].timestamp},${data[i].x},${data[i].y})`,
